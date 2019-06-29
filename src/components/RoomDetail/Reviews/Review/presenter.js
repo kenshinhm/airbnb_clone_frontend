ec5ab348d6b06498f1092012d2a63358474ef49d
jaehyoungyoo <kenshinhm@naver.com>
2019-06-29 22:59:06 +0900

    review time update

--- src/components/RoomDetail/Reviews/Review/presenter.js
+++ src/components/RoomDetail/Reviews/Review/presenter.js
@@ -3,10 +3,12 @@ import styles from './styles.scss';
 import * as PropTypes from "prop-types";
 import {wrapUrl} from "shared/utility.js";
 import {ReactComponent as Close} from 'svg/close.svg';
+import * as moment from 'moment';
 
 const Review = props => {
 
     const review = props.review;
+    const createTime = moment(new Date(props.review.create_time)).fromNow();
 
     return (
         <div className={styles.container}>
@@ -16,7 +18,7 @@ const Review = props => {
                 </img>
                 <div className={styles.info}>
                     <div className={styles.username}>{review.creator.username}</div>
-                    <span className={styles.time}>2019년 3월</span>
+                    <span className={styles.time}>{createTime}</span>
                 </div>
             </div>
             <div className={styles.message}>
@@ -38,6 +40,7 @@ const Review = props => {
 Review.propTypes = {
     review: PropTypes.shape({
         is_own: PropTypes.bool,
+        create_time: PropTypes.string,
         message: PropTypes.string,
         creator: PropTypes.shape({
             id: PropTypes.number,
